2fd4693f48c0315cb0d8585955dd08de7319b0b6
za4407809 <za4407809@gmail.com>
2024-11-12 05:59:56 -0800

    Add files via upload

---
+++ App.test.js
@@ -0,0 +1,15 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+import React from 'react';
+
+const About = () => {
+  return <h2>About Page</h2>;
+};
+
+export default About;
---
+++ dummy.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import About from './Components/About';
+import Navbar from './Components/Navbar';
+import Textform from './Components/Textform';
+// import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+function App(){
+ 
+  return (
+
+<>
+ <Navbar name="Textutilies App"about="about1"/>
+  <div className="container my-3">  
+  <Textform title="Enter The Text to Anaylze"/>
+   <About/>
+  </div>
+
+      </>
+
+     
+  );
+}
+export default App;
